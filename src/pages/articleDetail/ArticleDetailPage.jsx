6256996fd174c5aec2f164f6d2f3ba3b021341f7
gakgitufi <gakgitufi@gmail.com>
2023-05-22 21:25:55 +0800

    commit pertama

--- src/pages/articleDetail/ArticleDetailPage.jsx
+++ src/pages/articleDetail/ArticleDetailPage.jsx
@@ -42,6 +42,16 @@ const postsData = [
     },
 ]
 
+const tagsData = [
+    "Medical",
+    "Lifestyle",
+    "Learn",
+    "Healthy",
+    "Food",
+    "Diet",
+    "Education"
+]
+
 const ArticleDetailPage = () => {
   return (
     <MainLayout>
@@ -70,7 +80,7 @@ const ArticleDetailPage = () => {
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas purus viverra accumsan in nisl nisi. Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. In egestas erat imperdiet sed euismod nisi porta lorem mollis. Morbi tristique senectus et netus. Mattis pellentesque id nibh tortor id aliquet lectus proin. Sapien faucibus et molestie ac feugiat sed lectus vestibulum.
                 </p>
             </article>
-            <SuggestedPost header="Lastest Article" posts={postsData} />
+            <SuggestedPost header="Lastest Article" posts={postsData} tags={tagsData}/>
         </section>
     </MainLayout>
   )
--- src/pages/articleDetail/container/SuggestedPost.jsx
+++ src/pages/articleDetail/container/SuggestedPost.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-export default function SuggestedPost({ className, header, posts = [] }) {
+export default function SuggestedPost({ className, header, posts, tags = [] }) {
   return (
     <div className={`w-full shadow-[rgba(7,_65,_210,_0.1)_0px_9px_30px rounded-lg p-4 ${className}`}>
         <h2 className='font-roboto font-medium text-dark-hard'>
@@ -8,10 +9,26 @@ export default function SuggestedPost({ className, header, posts = [] }) {
         </h2>
         <div>
             {posts.map((item) => (
-                <div className='' key={item._id}>
-
+                <div className='flex space-x-3 flex-nowrap items-center' key={item._id}>
+                  <img src={item.image} alt='laptop' className='aspect-square object-cover rounded-lg w-1/5' />
+                  <div className='text-sm font-roboto text-dark-hard font-medium'>
+                    <h3>{item.title}</h3>
+                    <span className='text-xs opacity-60'>
+                      {new Date(item.createdAt).toLocaleDateString("en-US", {
+                        day: "numeric",
+                        month: "short",
+                        year: "numeric"
+                      })}
+                    </span>
+                  </div>
                 </div>
             ))}
+        </div>
+        <h2 className='flex flex-wrap gap-x-2 gap-y-2 mt-4'>Tags</h2>
+        <div>
+          {tags.map((item) => (
+            <Link className=''>{item}</Link>
+          ))}
         </div>
     </div>
   )
